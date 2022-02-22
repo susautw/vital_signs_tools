c5be7ab12c816b488c2fd1f4566fa52d69cd797e
susautw <susautw@gmail.com>
2022-02-22 18:04:57 +0800

    Refactor RollingAverage and Update document

--- src/utils.py
+++ src/utils.py
@@ -16,16 +16,16 @@ def test_time(name: str = "", line=None):
 
 class RollingAverage:
     values: deque
-    length: int
+    window_size: int
     low: float = -inf
     high: float = inf
     init: float = 0
     _rolling_avg: float = 0
 
-    def __init__(self, length: int, low: float = None, high: float = None, *, init: float = None):
+    def __init__(self, window_size: int, low: float = None, high: float = None, *, init: float = None):
         if init is not None:
             self.init = init
-        self.length = length
+        self.window_size = window_size
         if low is not None:
             self.low = low
         if high is not None:
@@ -38,7 +38,7 @@ class RollingAverage:
         """
         self.values.popleft()
         self.values.append(new_value)
-        self._rolling_avg = sum(self.values) / self.length
+        self._rolling_avg = sum(self.values) / self.window_size
         return self._apply_bound(self._rolling_avg)
 
     def current(self) -> float:
@@ -51,13 +51,19 @@ class RollingAverage:
         return min(max(value, self.low), self.high)
 
     def reset(self) -> None:
-        self.values = deque([self.init] * self.length)
+        """
+        re-init the instance
+        """
+        self.values = deque([self.init] * self.window_size)
         self._rolling_avg = self.init
 
-    def resize(self, new_length: int) -> None:
-        num_leak = max(0, new_length - self.length)
+    def resize(self, new_window_size: int) -> None:
+        """
+        resize window of the instance
+        """
+        num_leak = max(0, new_window_size - self.window_size)
         # slice will ignore ids out of bounds, e.g. (len(([0]*4)[-100:100]) == 4) is True
-        self.values = deque([self.init] * num_leak + list(self.values)[0: new_length])
-        self.length = new_length
+        self.values = deque([self.init] * num_leak + list(self.values)[0: new_window_size])
+        self.window_size = new_window_size
 
-        self._rolling_avg = sum(self.values) / self.length
+        self._rolling_avg = sum(self.values) / self.window_size
