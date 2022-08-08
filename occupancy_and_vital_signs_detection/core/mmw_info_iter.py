fb8222658599604160896880e068bfca6977f48f
Rin <susautw@gmail.com>
2022-08-08 22:11:36 +0800

    Complete hmap_only_mmw_info_iter

--- occupancy_and_vital_signs_detection/core/mmw_info_iter.py
+++ occupancy_and_vital_signs_detection/core/mmw_info_iter.py
@@ -1,11 +1,25 @@
 from typing import Iterator
 
+import numpy as np
+
 from . import MMWInfo
 
 
-class ArrayMMWInfoIterator(Iterator[MMWInfo]):
+class HMapOnlyMMWInfoIterator(Iterator[MMWInfo]):
+    hmaps: np.ndarray
+    i: int
 
-
+    def __init__(self, hmaps: np.ndarray):
+        self.hmaps = hmaps
+        self.i = 0
 
     def __next__(self) -> MMWInfo:
-        pass
+        if self.i >= len(self.hmaps):
+            raise StopIteration()
+        info = MMWInfo(
+            zones=[],
+            zone_decisions={},
+            hmap=self.hmaps[self.i]
+        )
+        self.i += 1
+        return info
